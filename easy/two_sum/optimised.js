d0e7f94becd9ece6fb05ff98834a985a4d096f11
esratjahanasha <esratasha.nwu@gmail.com>
2025-09-01 19:40:16 +0600

    brute force added

---
+++ easy/two_sum/optimised.js
@@ -0,0 +1,13 @@
+var twoSum = function (nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+};
+const r = twoSum([7, 11, 2, 15, 2], 9);
+console.log(r);
